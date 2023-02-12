7cf89113291d87faf9d6eaec83bd79fa5df38bcf
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-02-12 08:26:41 +0200

    Add files via upload

---
+++ network/end_points.dart
@@ -0,0 +1,14 @@
+const LOGIN='login';
+
+const REGISTER='register';
+
+const HOME='home';
+
+const SET_CATEGORY='categories';
+
+const FAVORITE='favorites';
+
+const PROFILE='profile';
+
+const UPDATE_PROFILE='update-profile';
+const search='products/search';
